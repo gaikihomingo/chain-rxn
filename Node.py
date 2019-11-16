5f4e220b44b99a9688fa2fec6c4bd121e45bb910
Amit Gaiki <xamit.94@gmail.com>
2019-11-17 01:56:13 +0530

    initial commit

---
+++ Node.py
@@ -0,0 +1,118 @@
+def get_type(x, y, width, height):
+    if x >= width or y >= height:
+        return 'invalid'
+    if x == 0 and y == 0:
+        return 'top_left'
+    elif x == 0 and y == height-1:
+        return 'bottom_left'
+    elif x == width-1 and y == 0:
+        return 'top_right'
+    elif x == width-1 and y == height-1:
+        return 'bottom_right'
+    elif x == 0:
+        return 'left_edge'
+    elif x == width-1:
+        return 'right_edge'
+    elif y == 0:
+        return 'top_edge'
+    elif y == height-1:
+        return 'bottom_edge'
+    else:
+        return 'box'
+
+
+def get_max_size(x, y, width, height):
+    node_type = get_type(x, y, width, height)
+    if node_type in ['top_left', 'bottom_left', 'top_right', 'bottom_right']:
+        return 2
+    elif node_type in ['left_edge', 'right_edge', 'top_edge', 'bottom_edge']:
+        return 3
+    elif node_type in ['box']:
+        return 4
+    else:
+        return 0
+
+
+def get_adjacent_node_coordinates(type, x, y):
+    if type == 'top_left':
+        return [(1, 0), (0, 1)]
+    elif type == 'top_right':
+        return [(x - 1, 0), (x, 1)]
+    elif type == 'bottom_left':
+        return [(0, y - 1), (1, y)]
+    elif type == 'bottom_right':
+        return [(x - 1, y), (x, y - 1)]
+    elif type == 'left_edge':
+        return [(0, y - 1), (0, y + 1), (1, y)]
+    elif type == 'right_edge':
+        return [(x, y - 1), (x, y + 1), (x - 1, y)]
+    elif type == 'top_edge':
+        return [(x - 1, 0), (x + 1, 0), (x, 1)]
+    elif type == 'bottom_edge':
+        return [(x - 1, y), (x + 1, y), (x, y - 1)]
+    elif type == 'box':
+        return [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
+
+
+def get_nodes_from_coordinates(node_list, coordinates):
+    nodes = []
+    for c in coordinates:
+        n = filter(lambda k: k.x == c[0] and k.y == c[1], node_list)
+        nodes.extend(n)
+    return nodes
+
+
+class Node:
+
+    def __init__(self, x, y, width, height, node_list):
+        self.x = x
+        self.y = y
+        self.type = get_type(x, y, width, height)
+        self.max_size = get_max_size(x, y, width, height)
+        self.current_size = 0
+        self.current_color = None
+        self.node_list = node_list
+
+    def add_no_turn(self, color):
+        if self.current_color is None or self.current_color == color:
+            self.current_color = color
+            self.current_size += 1
+            self.split_if_full()
+            return True
+        else:
+            return False
+
+    def capture(self, color):
+        self.current_color = color
+        self.current_size += 1
+
+    def split_if_full(self):
+        if self.current_size == self.max_size:
+            node_coordinates = get_adjacent_node_coordinates(self.type, self.x, self.y)
+            adjacent_nodes = get_nodes_from_coordinates(self.node_list, node_coordinates)
+            for node in adjacent_nodes:
+                node.capture(self.current_color)
+            self.current_size = 0
+            self.current_color = None
+            for node in adjacent_nodes:
+                node.split_if_full()
+
+    def get_adjacent_node_coordinates(self):
+        if self.type == 'top_left':
+            return [(1, 0), (0, 1)]
+        elif self.type == 'top_right':
+            return [(self.x-1, 0), (self.x, 1)]
+        elif self.type == 'bottom_left':
+            return [(0, self.y-1), (1, self.y)]
+        elif self.type == 'bottom_right':
+            return [(self.x-1, self.y), (self.x, self.y-1)]
+        elif self.type == 'left_edge':
+            return [(0, self.y-1), (0, self.y+1), (1, self.y)]
+        elif self.type == 'right_edge':
+            return [(self.x, self.y-1), (self.x, self.y+1), (self.x-1, self.y)]
+        elif self.type == 'top_edge':
+            return [(self.x-1, 0), (self.x+1, 0), (self.x, 1)]
+        elif self.type == 'bottom_edge':
+            return [(self.x-1, self.y), (self.x+1, self.y), (self.x, self.y-1)]
+        elif self.type == 'box':
+            return [(self.x, self.y-1), (self.x, self.y+1), (self.x-1, self.y), (self.x+1, self.y)]
---
+++ main.py
@@ -0,0 +1,53 @@
+from Node import Node
+width = 5
+height = 5
+
+def print_grid(node_list, width):
+    grid = [node_list[i:i + width] for i in range(0, len(node_list), width)]
+    for line in grid:
+        l = list(map(lambda n: {'size': n.current_size, 'color': n.current_color}, line))
+        print(l)
+
+node_list = []
+for i in range(width):
+    for j in range(height):
+        n = Node(i, j, width-1, height-1, node_list)
+        node_list.append(n)
+
+current = True
+while(1):
+    colors = ["red", "green"]
+    color = colors[0] if current else colors[1]
+    x = input("Grid number: ")
+    if node_list[int(x)].add_no_turn(color):
+        current = not current
+        print_grid(node_list, width)
+    else:
+        print("Incorrect input. try other number")
+# print(len(node_list))
+# print_grid(node_list, width)
+#
+# node_list[0].add_no_turn('red')
+# node_list[1].add_no_turn('green')
+# node_list[5].add_no_turn('red')
+# node_list[6].add_no_turn('green')
+# node_list[2].add_no_turn('red')
+# node_list[6].add_no_turn('green')
+# node_list[3].add_no_turn('red')
+# node_list[6].add_no_turn('green')
+# print("after play")
+# print_grid(node_list, width)
+# node_list[4].add_no_turn('red')
+# node_list[6].add_no_turn('green')
+# node_list[0].add_no_turn('red')
+# node_list[13].add_no_turn('green')
+# node_list[14].add_no_turn('red')
+# node_list[15].add_no_turn('green')
+# node_list[16].add_no_turn('red')
+# node_list[17].add_no_turn('green')
+# print("after play")
+# print_grid(node_list, width)
+
+
+
+
